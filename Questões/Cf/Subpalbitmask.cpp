6063c56711a01e439f33b5530488d9f2174ecb90
tiagocbr <tiagocbr06@hotmail.com>
2023-07-05 02:13:00 -0300

    Questao maratona mineira palindromos

---
+++ Questões/Cf/Subpalbitmask.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = (1 << 8) + 20;
+int qtd[maxn];
+int val[8];
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    qtd[0] = 1;
+    int mask = 0;
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        mask ^= (1 << (s[i] - 'a'));
+        res += qtd[mask];
+
+        for (int j = 0; j < 8; j++)
+        {
+            res += qtd[mask ^ (1 << j)];
+        }
+        qtd[mask]++;
+    }
+    cout << res;
+}
